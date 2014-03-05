d512d58f2c179560f29055df6cd59387114fd5d5
Patrick <patrickelectric@gmail.com>
2014-03-05 17:46:30 -0300

    Adicionado comentarios importantes

--- ArduLink/ArduLink.h
+++ ArduLink/ArduLink.h
@@ -1,26 +1,44 @@
-//Class to make comunication with simulink
+/**
+  ******************************************************************************
+  * @file    ArduLink.h
+  * @author  Patrick José Pereira
+  * @version V1.2
+  * @date    5-Março-2014
+  * @brief   A library to make comunication with simulink
+  *
+  * By Default:
+  *		Terminator 		"\0"
+  *		Header  		NONE
+  *
+  * Is advisable that after burn the Arduino, restart him befone the Simulink comunication.
+  *		
+  * The transmission's order of the bytes is BigEndian.
+  *
+  * If the Terminator, Header, Baudrate. vector's size and the
+  * vector's format (int32 or single) changed,  REMEMBER to modifie in the
+  * Simulink.
+  *
+  * Vector's size = ['vector's size' 1] in Simulink.
+  *
+  * @Todo Integrate function that send matrices.
+  ******************************************************************************/
 
-//Terminator NULL
-//Data Size ["Vector size" 1]
-
-//Byte order BigEndian
 class ArduLink
 {
 	private:
 		char* Terminator;
 		char Header;
 		char buffer[4];		//Buffer 8 bits
-		int vector_size;	//Vector size
+		int vector_size;	//Vector's size
 
 		char char2hex(char x);
 		void serialFloatPrint(float f); 
 		void sendTerminator();
 		void sendHeader();
 	public:
-		// start the class  (baudrate , vector size)
 		void Init(int baudrate, int size);	
 		void Init(int baudrate, int size, char* term);	
-		void Init(int baudrate, int size, char head, char* term);
+		void Init(int baudrate, int size, char* term, char head);
 
 		//send buffer to simulink
 		///int32
@@ -32,6 +50,14 @@ class ArduLink
 		void SendBuffer(float* vector); 		
 };
 
+/** \brief Starts the initials values and parameters. 
+  *
+  * @param  baudrate  Define the baud rate
+  * @param 	size      Define the vector's size
+  * @param 	term      Define the Simulink's Terminator 
+  * @param 	head      Define the Simulink's Header
+  * @retval NONE
+  */
 void ArduLink::Init(int baudrate, int size)	
 {
 	vector_size=size;
@@ -48,7 +74,7 @@ void ArduLink::Init(int baudrate, int size,char* term)
 	Serial.begin(baudrate);
 };
 
-void ArduLink::Init(int baudrate, int size, char head, char* term)	
+void ArduLink::Init(int baudrate, int size, char* term, char head)	
 {
 	vector_size=size;
 	Header=head;
@@ -56,6 +82,12 @@ void ArduLink::Init(int baudrate, int size, char head, char* term)
 	Serial.begin(baudrate);
 };
 
+/** \brief Send the float vector to serial.
+  * It can be a single float of an array
+  *
+  * @param  vector    Indicates the initial address of the vector
+  * @retval NONE
+  */
 void ArduLink::SendBuffer(float* vector)
 {
 	sendHeader();
@@ -71,6 +103,12 @@ void ArduLink::SendBuffer(float vector)
 	sendTerminator();
 };
 
+/** \brief Send the int vector to serial.
+  * It can be a single int of an array
+  *
+  * @param  vector    Indicates the initial address of the vector
+  * @retval NONE
+  */
 void ArduLink::SendBuffer(int* vector)
 {
 	sendHeader();
@@ -103,6 +141,11 @@ void ArduLink::SendBuffer(int vector)
 	sendTerminator();
 };
 
+/** \brief Transform a letter in a hexadecimal value
+  *
+  * @param  x          the char that have 0 to A
+  * @retval return a char that have the value 0x00 to 0x0A
+  */
 char ArduLink::char2hex(char x)
 {
 	switch (x) 
@@ -159,19 +202,30 @@ char ArduLink::char2hex(char x)
   	}
 }
 
-//http://forum.arduino.cc/index.php?topic=112597.0
-//BigEndian
+/** \brief Transform a float to SINGLE and send by the serial in BigEndian
+  * found in http://forum.arduino.cc/index.php?topic=112597.0
+  *
+  * @param  f          the float number
+  */
 void ArduLink::serialFloatPrint(float f) 
 {
 	byte * b = (byte *) &f;
   	Serial.write(b,4);
 }
 
+/** \brief Send the terminator by the serial
+  *
+  * @param  NONE
+  */
 void ArduLink::sendTerminator()
 {
 	Serial.write(Terminator);	
 }
 
+/** \brief Send the Header by the serial
+  *
+  * @param  NONE
+  */
 void ArduLink::sendHeader()
 {
 	if(Header!=0x00)
