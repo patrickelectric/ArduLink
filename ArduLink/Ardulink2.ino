5b60f651168904c739986a352da95188673652a4
Patrick <patrickelectric@gmail.com>
2014-10-11 12:14:13 -0300

    realizando a leitura do matlab todo implementar metodo eficaz de leitura do buffer

--- ArduLink/Ardulink2.ino
+++ ArduLink/Ardulink2.ino
@@ -29,58 +29,29 @@ void ringbyteback(byte* buffer,int n, int _size, byte* _byte)
   for(int i=0; i<_size+n; i++)
   {
     _byte[i]=buffer[i+n];
-    //Serial.println(i);
+    //soSerial.println(i);
   }
 }
 
-//SoftwareSerial soSerial(10, 11); // RX, TX
+SoftwareSerial soSerial(10, 11); // RX, TX
 
 
 void setup()
 {
-  //soSerial.begin(9600);
+  soSerial.begin(115200);
   Serial.begin(9600);
+  soSerial.println(">------------<");
 }
 
 void loop()
 {
-  //Serial.println("--------------");
-  //Serial.print("->");
-  //Serial.println(Serial.available());
-  //Serial.print(" ");
-  //Serial.println(char(Serial.peek()));
-  while(Serial.peek()=='L' && Serial.available()>24)
+  while(Serial.peek()=='L' || Serial.available()!=0)
   {
-    Serial.read();
-    Serial.println("Start byte !");
-
-    int i=0;
-    byte buffer[32]={0};
-    for(i=0; Serial.peek()!='\n'; i++)
-      buffer[i]=Serial.read();
-    Serial.read();//\n
-    Serial.print("S-E = "); Serial.println(i);
-    float b;
-    if(i!=24) break;
-
-    b=byte2float(buffer);
-    Serial.println(b);
-
-    for(int i=0; i<8; i++)
-    {
-      ringbyteback(buffer,4,32,buffer);
-      b=byte2float(buffer);
-      Serial.println(b);
-    }
+    if(Serial.peek() == '\n')
+      soSerial.print("END");
+    soSerial.println("--------------");
+    soSerial.print((char)(Serial.peek()));
+    soSerial.print(" - ");
+    soSerial.println((Serial.read()),HEX);
   } 
-  if(Serial.available()>24)
-    Serial.read();
-  /*
-  else
-  if((Serial.available()!=0 && Serial.available()<36))
-  {
-    Serial.println(Serial.read());
-    //Serial.flush();
-  }*/
-  
 }
